79c2c56865594b3b12e5df5620a4b4529c525444
Abigail Pitcairn <abigail.pitcairn@maine.edu>
2024-11-06 15:48:53 -0500

    Files outline

---
+++ Calc.py
@@ -0,0 +1,6 @@
+import re
+
+# Calculation methods to support basic integer arithmetic with variables
+# Author: Abigail Pitcairn
+# Version: 11.06.24
+
---
+++ Main.py
@@ -0,0 +1,5 @@
+import re
+
+# Main method to read in user input and call calculation methods
+# Author: Abigail Pitcairn
+# Version: 11.06.24
